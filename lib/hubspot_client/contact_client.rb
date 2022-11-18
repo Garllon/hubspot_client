031028d8aa646b66fc9ad51d9e61d74a166e5cdc
Bennet Palluthe <bennet.palluthe@protonmail.com>
2022-11-18 08:15:42 +0100

    add find properties
    we need some more fields then only the defailt once

--- lib/hubspot_client/contact_client.rb
+++ lib/hubspot_client/contact_client.rb
@@ -7,14 +7,19 @@ module HubspotClient
   class ContactClient
     include HTTParty
     base_uri 'https://api.hubapi.com'
+
     BASE_PATH = "/crm/v3/objects/contacts"
+    FIND_PROPERTIES = %w(firstname lastname email associatedcompanyid).freeze
 
     def find_by_email(email)
-      find_by("#{BASE_PATH}/#{email}?idProperty=email")
+      query_params = find_query_params({idProperty: 'email'})
+      find_by("#{BASE_PATH}/#{email}?#{query_params}")
     end
 
     def find_by_id(hubspot_id)
-      find_by("#{BASE_PATH}/#{hubspot_id}")
+      query_params = find_query_params
+
+      find_by("#{BASE_PATH}/#{hubspot_id}?#{query_params}")
     end
 
     def create(properties)
@@ -44,6 +49,13 @@ module HubspotClient
       return response if response.code == 200
 
       raise ContactNotFound, 'Hubspot Contact Not Found'
+    end
+
+    def find_query_params(query_params = {})
+      query_params['properties'] = FIND_PROPERTIES.join(',')
+      query_params.map do |key, value|
+        "#{key}=#{value}"
+      end.join('&')
     end
 
     def headers
