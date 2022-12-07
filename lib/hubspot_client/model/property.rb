fd98c9f4177ccf0da968bdcb537a22c6a5f2e873
Roman Konz <roman@konz.me>
2022-12-07 08:25:51 +0100

    only update writeable properties

--- lib/hubspot_client/model/property.rb
+++ lib/hubspot_client/model/property.rb
@@ -1,19 +1,20 @@
 # frozen_string_literal: true
 
 module HubspotClient
-  module Service
-    class Model
+  module Model
+    class Property
+      def self.all_for(object_type)
+        HubspotClient::Client::Properties.new.for(object_type)
+      end
+
       def self.writable_property_names_for(object_type)
-        readonly_properties = self.for(object_type)['results'].reject do |property|
+        readonly_properties = self.all_for(object_type)['results'].reject do |property|
           property['modificationMetadata']['readOnlyValue'] == true
         end
 
         readonly_properties.map { |property| property['name'].to_sym }
       end
 
-      def self.for(object_type)
-        HubspotClient::Client::Properties.new.for(object_type)
-      end
     end
   end
 end
